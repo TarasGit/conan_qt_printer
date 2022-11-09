6c1b2dc1fff7eb672a26cd7eae193d43a6330d15
T <test@test.test>
2022-11-09 21:25:17 +0100

    Added resource directory

--- qt_conan_de_DE.ts
+++ qt_conan_de_DE.ts
@@ -5,31 +5,57 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <location filename="qt_conan_autogen/include/ui_mainwindow.h" line="75"/>
         <source>MainWindow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="27"/>
-        <location filename="qt_conan_autogen/include/ui_mainwindow.h" line="76"/>
         <source>Download</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="40"/>
-        <location filename="qt_conan_autogen/include/ui_mainwindow.h" line="77"/>
         <source>Download Conan from GitHub</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="63"/>
-        <location filename="qt_conan_autogen/include/ui_mainwindow.h" line="78"/>
         <source>Quit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="17"/>
+        <location filename="mainwindow.cpp" line="82"/>
         <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="96"/>
+        <source>&amp;New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="98"/>
+        <source>Create a new file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="102"/>
+        <source>&amp;Open...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="104"/>
+        <source>Open an existing file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="108"/>
+        <source>&amp;Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="110"/>
+        <source>Save the document to disk</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
