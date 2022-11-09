052625b54545830e7d305ff6cfb0742fcc639f04
T <test@test.test>
2022-11-09 21:37:24 +0100

    Cleanup comments

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -78,38 +78,30 @@ void MainWindow::on_button_quit_clicked()
 
 void MainWindow::createMenus()
 {
-//! [9] //! [10]
     fileMenu = menuBar()->addMenu(tr("&File"));
     fileMenu->addAction(newAct);
-//! [9]
     fileMenu->addAction(openAct);
-//! [10]
     fileMenu->addAction(saveAct);
-//! [11]
     fileMenu->addSeparator();
 }
 
 
 void MainWindow::createActions()
 {
-//! [5]
     newAct = new QAction(tr("&New"), this);
     newAct->setShortcuts(QKeySequence::New);
     newAct->setStatusTip(tr("Create a new file"));
     connect(newAct, &QAction::triggered, this, &MainWindow::newFile);
-//! [4]
 
     openAct = new QAction(tr("&Open..."), this);
     openAct->setShortcuts(QKeySequence::Open);
     openAct->setStatusTip(tr("Open an existing file"));
     connect(openAct, &QAction::triggered, this, &MainWindow::open);
-//! [5]
 
     saveAct = new QAction(tr("&Save"), this);
     saveAct->setShortcuts(QKeySequence::Save);
     saveAct->setStatusTip(tr("Save the document to disk"));
     connect(saveAct, &QAction::triggered, this, &MainWindow::save);
-
 }
 
 void MainWindow::newFile(){
